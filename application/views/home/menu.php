b8f596fd0054946c4f02df5942e6451aefb9b586
Bisbiss <01newbi@gmail.com>
2019-10-09 23:48:24 +0700

    Edit menu donatur

--- application/views/home/menu.php
+++ application/views/home/menu.php
@@ -1,19 +1,19 @@
 <!-- Just an image -->
 <nav class="navbar bg-light">
 	<div class="container">
-	  <a class="navbar-brand" href="<?php echo base_url() ?>">
+	  <a class="navbar-brand" href="<?php echo base_url('donatur') ?>">
 	    <img src="<?php echo base_url('assets/dist/img/logo.png') ?>" class="img-fluid" width="10%"><span class="text-info">isbiss</span>
 	  </a>
 
 	  	<ul class="nav justify-content-end">
 		  <li class="nav-item">
-		    <a class="nav-link" href="#">Donasi</a>
+		    <a class="nav-link" href="donatur/donasi">Donasi</a>
 		  </li>
 		  <li class="nav-item">
-		    <a class="nav-link" href="#">Profil</a>
+		    <a class="nav-link" href="donatur/profil">Profil</a>
 		  </li>
 		  <li class="nav-item">
-		    <a class="nav-link" href="#">Bantuan</a>
+		    <a class="nav-link" href="donatur/bantuan">Bantuan</a>
 		  </li>
 		  <li class="nav-item">
 		    <a class="nav-link" href="<?php echo base_url('welcome/logout')?> ">Keluar</a>
