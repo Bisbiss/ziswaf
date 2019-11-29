720ebbb0deeade649804fbc7349777cb54ef3091
Bisbiss <01newbi@gmail.com>
2019-11-29 10:52:44 +0700

    bantuan

---
+++ application/views/home/bantuan.php
@@ -0,0 +1,15 @@
+<section class="content">
+    <div class="container-fluid">
+      <div class="container">
+        <!-- Pesan Selamat Datang -->
+        <div class="row">
+            <div class="col-md">
+                <br>
+                <div class="callout callout-info">
+                    <h3 class="card-title">Bantuan</h3>
+                </div>
+            </div>
+       </div>
+      </div>
+    </div>
+</section>
