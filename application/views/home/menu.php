8d3518e9c841e233c320f90e722953955ba157f9
Bisbiss <01newbi@gmail.com>
2020-04-23 11:48:19 +0700

    file brand

--- application/views/home/menu.php
+++ application/views/home/menu.php
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-expand-lg bg-light">
 	<div class="container">
 		<a class="navbar-brand" href="<?php echo base_url('home') ?>">
-			<img src="<?php echo base_url('assets/dist/img/logo-menu.png') ?>" class="img-fluid" width="20%">
+			<img src="<?php echo base_url('assets/dist/img/brand.png') ?>" class="img-fluid">
 		</a>
 	  	<button class="navbar-toggler bg-info" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
